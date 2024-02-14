fae872f9f0810530091bd9a3bf4e950c6f003acb
wbham <wbham97@naver.com>
2024-02-14 15:16:10 +0900

    initialsetting

---
+++ app/src/main/java/org/shop/facerecog/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.shop.facerecog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.shop.facerecog.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
